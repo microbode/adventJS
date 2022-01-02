2cc3116cea830cfe232b09b9a061ae133205654c
David Vázquez <david.vazquez.alonso@gmail.com>
2022-01-02 03:00:25 +0100

    Reto 23

--- index.js
+++ index.js
@@ -1,33 +1,5 @@
-import countDecorations from "./22.js";
+import canReconfigure from "./23.js";
 
-const bigTree = {
-  value: 1,
-  left: {
-    value: 5,
-    left: {
-      value: 7,
-      left: {
-        value: 3,
-        left: null,
-        right: null,
-      },
-      right: null,
-    },
-    right: null,
-  },
-  right: {
-    value: 6,
-    left: {
-      value: 5,
-      left: null,
-      right: null,
-    },
-    right: {
-      value: 1,
-      left: null,
-      right: null,
-    },
-  },
-};
-
-console.log(countDecorations(bigTree));
+const from = "MMM";
+const to = "MID";
+console.log(canReconfigure(from, to)); // true
